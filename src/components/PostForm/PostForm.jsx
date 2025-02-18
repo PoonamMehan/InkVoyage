cb38fb8674ef2a13e0ebaf9aa92d86d393c9ae63
PoonamMehan <poonammehan655@gmail.com>
2025-02-18 18:46:20 +0530

    Fixed the place of spinner

--- src/components/PostForm/PostForm.jsx
+++ src/components/PostForm/PostForm.jsx
@@ -229,7 +229,7 @@ function PostForm() {
             </div>
         }
         {submitLoadingStarted &&
-          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[80px]">
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1.5">
             <Spinner size="3" />
           </div>
            
